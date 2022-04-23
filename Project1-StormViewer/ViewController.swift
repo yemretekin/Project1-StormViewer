5b2b670eb805cb7e93f4855651a4f885272d5bbe
hilaldayal <hilaldayal@hotmail.com>
2022-04-23 06:09:15 +0300

    first committ

---
+++ Project1-StormViewer/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Project1-StormViewer
+//
+//  Created by Emre Tekin on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+
+
+}
